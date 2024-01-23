51f895ff34be9b25b6baf8fdb5bd6896b0215758
A00448976 <A00448976@T240-09>
2024-01-23 10:22:36 -0600

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello!" << endl;
+	cout << My name is Celeste! << endl;
+	cout << "I hope to learn a lot in this class." << endl;
+		 
+}
